609bbbe0d9e7b3c2ec074e2c791377a2656581a6
Alex Novelli <alexnovelli@gmail.com>
2018-10-26 12:05:45 -0300

    popover quase final

--- vanilla-popover.js
+++ vanilla-popover.js
@@ -1,85 +1,151 @@
- window.onload=function(){
-    let popoverObject={};
-    popoverObject.verifyPopover=function(){
-      popoverObject.element=document.querySelectorAll(".vanilla-popover-ico");
-        if(popoverObject.element != undefined){
-          return popoverObject;
-        }
-    }
-    popoverObject.setPosition=function(){
-      for(let i=0; i<popoverObject.element.length; i++){
-        popoverObject.element[i].addEventListener('mouseover',function(){
-          let popover=popoverObject.element[i];
-          let pos=popover.getBoundingClientRect();
-          let tela=window.innerWidth;
-          let popoverId=popover.getAttribute("id");
+ window.onload = function () {
+   let popoverObject = {};
+   popoverObject.verifyPopover = function () {
+     popoverObject.element = document.querySelectorAll(".vanilla-popover-ico");
+     if (popoverObject.element != undefined) {
+       return popoverObject;
+     }
+   }
+   popoverObject.setPosition = function () {
+     for (let i = 0; i < popoverObject.element.length; i++) {
+       popoverObject.element[i].addEventListener('mouseover', function () {
+         let popover = popoverObject.element[i];
+         let pos = popover.getBoundingClientRect();
+         let telaW = window.innerWidth;
+         let telaH = window.innerHeight;
+         let popoverId = popover.getAttribute("id");
 
-            console.log(pos);
-            console.log(tela);
-            console.log(pos.left);
-            console.log(pos.top);
+         let posLeft = pos.left < telaW / 3;
+         let posCenterW = pos.left > telaW / 3 && pos.left < (telaW / 3) * 2;
+         let posRight = pos.left > (telaW / 3) * 2;
+         let posBottom = pos.top > (telaH / 3) * 2;
 
-            let popoverContainer = document.querySelector("div[data-popover=" + popoverId + "]");
+         console.log(pos);
+         console.log(telaW);
+         console.log("telaH",telaH);
+         console.log(pos.left);
+         console.log(pos.top);
 
-              if(tela > 467){
-                if(pos.left < tela/3 && tela > 467){
-                  console.log("popoverEsquerda");
-                  popoverContainer.classList.add('popoverEsquerda');
+         let popoverContainer = document.querySelector("div[data-popover=" + popoverId + "]");
 
-                  let top=pos.top + 15 +'px';
-                  let left=pos.left + 'px';
-                  popoverContainer.style.top = top;
-                  popoverContainer.style.left = left;
+         if (posBottom) {
+           if (telaW > 768) {
+             if (posLeft) {
+               console.log("popoverEsquerda");
 
-                  popoverContainer.style.display='block';
-                };
+               popoverContainer.style.display = 'block';
+               let popoverContHeight = popoverContainer.offsetHeight + 9; // 25px distancia - 16px do icone
 
-                if(pos.left > tela/3 && pos.left < (tela/3)*2 || tela < 467){
-                  console.log("popoverCentro");
-                  popoverContainer.classList.add('popoverCentro');
+               let top = pos.top - popoverContHeight + 'px';
+               let left = pos.left + 25 + 'px';
+               popoverContainer.style.top = top;
+               popoverContainer.style.left = left;
 
-                  let top=pos.top + 15 +'px';
-                  let left=pos.left - 210 + 'px';
-                  popoverContainer.style.top = top;
-                  popoverContainer.style.left = left;
+             };
 
-                  popoverContainer.style.display='block';
-                }
+             if (posCenterW) {
+               console.log("popoverCentro");
 
-                if(pos.left > (tela/3)*2 && tela > 467){
-                  console.log("popoverDireita");
-                  popoverContainer.classList.add('popoverDireita');
+               popoverContainer.style.display = 'block';
+               let popoverContWidth = popoverContainer.offsetWidth / 2 - 8; // valor 8 se refere a metade do tamanho do icone
+               let popoverContHeight = popoverContainer.offsetHeight + 9; // 25px distancia - 16px do icone
 
-                  let top=pos.top + 15 +'px';
-                  let left=pos.left - 450 + 'px';
-                  popoverContainer.style.top = top;
-                  popoverContainer.style.left = left;
+               let top = pos.top - popoverContHeight + 'px';
+               let left = pos.left - popoverContWidth + 'px';
+               popoverContainer.style.top = top;
+               popoverContainer.style.left = left;
 
-                  popoverContainer.style.display='block';
-                }
-              }else{
-                console.log("popoverMobile");
-                popoverContainer.classList.add('popoverMobile');
+             }
 
-                let top=pos.top + 15 +'px';
-                popoverContainer.style.display='block';
-              }
+             if (posRight) {
+               console.log("popoverDireita");
 
+               popoverContainer.style.display = 'block';
+               let popoverContWidth = popoverContainer.offsetWidth;
+               let popoverContHeight = popoverContainer.offsetHeight + 9; // 25px distancia - 16px do icone
 
-        })
-      }
+               let top = pos.top - popoverContHeight + 'px';
+               let left = pos.left - popoverContWidth + 'px';
+               popoverContainer.style.top = top;
+               popoverContainer.style.left = left;
 
-      return popoverObject;
-    }
-    popoverObject.hide=function(){
-    for(let i=0; i<popoverObject.element.length; i++){
-          popoverObject.element[i].addEventListener('mouseout',function(){
-              let popover=popoverObject.element[i];
-            let popoverId=popover.getAttribute("id");
-            let popoverContainer = document.querySelector("div[data-popover=" + popoverId + "]");
-            popoverContainer.style.display='none';
-          })
-    }
-    }
-    popoverObject.verifyPopover().setPosition().hide();
-  }
+             }
+
+           } else {
+             console.log("popoverMobile");
+
+             popoverContainer.style.display = 'block';
+
+             let popoverContHeight = popoverContainer.offsetHeight + 15; // 25px distancia - 16px do icone
+             let top = pos.top - popoverContHeight + 'px';
+             popoverContainer.style.top = top;
+
+           }
+         } else {
+           if (telaW > 768) {
+             if (posLeft) {
+               console.log("popoverEsquerda");
+
+               popoverContainer.style.display = 'block';
+
+               let top = pos.top + 25 + 'px';
+               let left = pos.left + 25 + 'px';
+               popoverContainer.style.top = top;
+               popoverContainer.style.left = left;
+
+             };
+
+             if (posCenterW) {
+               console.log("popoverCentro");
+
+               popoverContainer.style.display = 'block';
+               let popoverContWidth = popoverContainer.offsetWidth / 2 - 8; // valor 8 se refere a metade do tamanho do icone
+
+               let top = pos.top + 25 + 'px';
+               let left = pos.left - popoverContWidth + 'px';
+               popoverContainer.style.top = top;
+               popoverContainer.style.left = left;
+
+             }
+
+             if (posRight) {
+               console.log("popoverDireita");
+
+               popoverContainer.style.display = 'block';
+               let popoverContWidth = popoverContainer.offsetWidth;
+               console.log(popoverContWidth);
+
+               let top = pos.top + 25 + 'px';
+               let left = pos.left - popoverContWidth + 'px';
+               popoverContainer.style.top = top;
+               popoverContainer.style.left = left;
+
+             }
+
+           } else {
+             console.log("popoverMobile");
+
+             popoverContainer.style.display = 'block';
+
+             let top = pos.top + 15 + 'px';
+             popoverContainer.style.top = top;
+           }
+         }
+
+       });
+     }
+
+     return popoverObject;
+   }
+   popoverObject.hide = function () {
+     for (let i = 0; i < popoverObject.element.length; i++) {
+       popoverObject.element[i].addEventListener('mouseout', function () {
+         let popover = popoverObject.element[i];
+         let popoverId = popover.getAttribute("id");
+         let popoverContainer = document.querySelector("div[data-popover=" + popoverId + "]");
+         popoverContainer.style.display = 'none';
+       })
+     }
+   }
+   popoverObject.verifyPopover().setPosition().hide();
+ }
